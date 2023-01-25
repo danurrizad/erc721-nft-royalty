ee486f674c757f8627252d3eff586b399fd030cd
Danurrizad Ryan Darmoko <79238162+danurrizad@users.noreply.github.com>
2023-01-25 14:41:28 +0700

    changing library to fix royalty issue

--- contracts/ERC721-NFT.sol
+++ contracts/ERC721-NFT.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.8.1/security/Pausable.sol";
-import "@openzeppelin/contracts@4.8.1/access/Ownable.sol";
-import "@openzeppelin/contracts@4.8.1/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/common/ERC2981.sol";
 
-import "rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
-import "rarible/royalties/contracts/LibPart.sol";
-import "rarible/royalties/contracts/LibRoyaltiesV2.sol";
 
-contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, RoyaltiesV2Impl {
+contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC2981 {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -22,6 +19,7 @@ contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Pa
     uint256 public MAX_MINT_AMOUNT = 1;
     string baseURI;
     //baseURI = "ipfs://QmXvtwWe6yZPmH4sBaYv2rVC3voWMy2PfsbqeD9ytMmYzz/";
+    //mint price = "25000000000000000" wei
 
      bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
 
@@ -36,20 +34,23 @@ contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Pa
         baseURI = _newBaseURI;
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+ 
 
     function safeMint(address to) public payable{
         require(totalSupply() < MAX_SUPPLY, "Can't mint this NFT anymore");
         require(msg.value >= MINT_PRICE, "Ether is not enough to send");
-        uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
+    }
+
+    function mintWithRoyalty(address receiver, address royaltyFeeReceiver, uint96 royaltyFee) public payable{
+        require(totalSupply() < MAX_SUPPLY, "Can't mint this NFT anymore");
+        require(msg.value >= MINT_PRICE, "Ether is not enough to send");
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(receiver, tokenId);
+        _setTokenRoyalty(tokenId, royaltyFeeReceiver, royaltyFee);
     }
 
     //function withdraw
@@ -84,43 +85,23 @@ contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Pa
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
-        whenNotPaused
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    //configure royalties for Rariable
-    function setRoyalties(uint _tokenId, address payable _royaltiesRecipientAddress, uint96 _percentageBasisPoints) public onlyOwner {
-        LibPart.Part[] memory _royalties = new LibPart.Part[](1);
-        _royalties[0].value = _percentageBasisPoints;
-        _royalties[0].account = _royaltiesRecipientAddress;
-        _saveRoyalties(_tokenId, _royalties);
-    }
 
-
-    //configure royalties for Mintable using the ERC2981 standard
-    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount) {
-      //use the same royalties that were saved for Rariable
-      LibPart.Part[] memory _royalties = royalties[_tokenId];
-      if(_royalties.length > 0) {
-        return (_royalties[0].account, (_salePrice * _royalties[0].value) / 10000);
-      }
-      return (address(0), 0);
-    }
-
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
-        if(interfaceId == LibRoyaltiesV2._INTERFACE_ID_ROYALTIES) {
-            return true;
-        }
-
-        if(interfaceId == _INTERFACE_ID_ERC2981) {
-          return true;
-        }
-
+    
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC2981)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
+
+    
 
 
     // The following functions are overrides required by Solidity.
@@ -138,14 +119,5 @@ contract VirtualRealityBoyNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Pa
         return super.tokenURI(tokenId);
     }
 
-    /*
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-    */
+    
 }
